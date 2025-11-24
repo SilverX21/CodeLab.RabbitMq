7f5734dff756df466bc8d900388b0dabf67ff827
SilverX21 <nuno21araujo95@gmail.com>
2025-11-24 00:36:21 +0000

    Add project files.

---
+++ CodeLab.RabbitMq.Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+//here we declare the queue we want to use to publish our messages
+await channel.QueueDeclareAsync(queue: "messages", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine("Waiting for messages...");
+
+var consumer = new AsyncEventingBasicConsumer(channel); //here we create the consumer
+
+//this event will be triggered when a message is received
+//this is the consumer delegate that will process the messages
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Received: {message}");
+
+    await ((AsyncEventingBasicConsumer)sender).Channel.BasicAckAsync(eventArgs.DeliveryTag, multiple: false);
+};
+
+//here we start consuming messages from the queue
+await channel.BasicConsumeAsync(queue: "messages", autoAck: false, consumer: consumer);
+
+Console.ReadLine();
---
+++ CodeLab.RabbitMq.Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+//here we declare the queue we want to use to publish our messages
+await channel.QueueDeclareAsync(queue: "messages",
+    durable: true,  //if it's true, the queue will survive a broker restart, otherwise, they will be deleted, leading to data loss
+    exclusive: false, //this means that the queue is exclusive to the connection that declared it
+    autoDelete: false, //if it's true, the queue will be deleted when there are no more consumers
+    arguments: null);
+
+await Task.Delay(10_000);
+
+for (int i = 0; i < 10; i++)
+{
+    string message = $"{DateTime.UtcNow} - {Guid.CreateVersion7()}";
+    var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(exchange: string.Empty,
+        routingKey: "messages", //this is the queue name we want to publish our message to
+        mandatory: true,
+        basicProperties: new BasicProperties { Persistent = true },
+        body: body);
+
+    Console.WriteLine($"Sent: {message}");
+
+    await Task.Delay(2000);
+}
